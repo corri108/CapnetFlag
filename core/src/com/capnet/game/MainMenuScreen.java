e52cda2b4d999f6b1ccb7e5b9581acf43a2f918a
William Corrin <corri108@mail.chapman.edu>
2016-04-21 15:57:42 -0700

    made initial menu

--- core/src/com/capnet/game/MainMenuScreen.java
+++ core/src/com/capnet/game/MainMenuScreen.java
@@ -8,7 +8,11 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Window;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 
 /**
  * Created by michaelpollind on 4/19/16.
@@ -22,16 +26,65 @@ public class MainMenuScreen implements Screen {
     private Stage _stage;
     private Main main;
 
+    //main window
+    TextButton button;
+    TextField namefield;
+    TextField ipfield;
+    TextField portfield;
+    BitmapFont font;
+    TextButtonStyle textButtonStyle;
+    Dialog dialog;
+
     public  MainMenuScreen(Main main)
     {
         this.main = main;
 
         _stage = new Stage();
         Skin uiSkin = new Skin(Gdx.files.internal("uiskin.json"));
+        font = new BitmapFont();
+        //make menu takeup the whole screen
+        dialog = new Dialog("Capnet Flag : Menu", uiSkin);
+        dialog.setPosition(0,600);
+        dialog.setWidth(640);
+        dialog.setHeight(480);
+        //end menu window
 
-        Dialog dialog = new Dialog("test", uiSkin);
+        //textButtonStyle
+        textButtonStyle = new TextButtonStyle();
+        textButtonStyle.font = font;
+        textButtonStyle.up = uiSkin.getDrawable("default-round");
+        textButtonStyle.down = uiSkin.getDrawable("default-round-down");
+        textButtonStyle.checked = uiSkin.getDrawable("default-round");
+        //end style
 
+        //button connect
+        button = new TextButton("Connect", textButtonStyle);
+        button.setPosition(270,125);
+        button.setWidth(100);
+        button.setHeight(40);
+        //end button
+
+        //fields of text
+        namefield = new TextField("Enter Name:", uiSkin);
+        namefield.setPosition(270,300);
+        namefield.setWidth(100);
+        namefield.setHeight(40);
+        ipfield = new TextField("Enter IP:", uiSkin);
+        ipfield.setPosition(270,250);
+        ipfield.setWidth(100);
+        ipfield.setHeight(40);
+        portfield = new TextField("Enter Port:", uiSkin);
+        portfield.setPosition(270,200);
+        portfield.setWidth(100);
+        portfield.setHeight(40);
+        //end text fields
+
+        //add all of our objects to the render list
         _stage.addActor(dialog);
+        _stage.addActor(button);
+        _stage.addActor(namefield);
+        _stage.addActor(ipfield);
+        _stage.addActor(portfield);
     }
 
     @Override
