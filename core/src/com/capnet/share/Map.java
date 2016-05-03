93f4ebd5aec53fa488a76cf807fb524f677bb72a
Michael Pollind <mpollind@gmail.com>
2016-05-03 16:44:37 -0700

    added starting region

--- core/src/com/capnet/share/Map.java
+++ core/src/com/capnet/share/Map.java
@@ -3,8 +3,11 @@ package com.capnet.share;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
+import com.capnet.share.Entities.Player;
 import com.capnet.share.packets.ByteHelper;
+import com.capnet.share.packets.GameState;
 import com.capnet.share.packets.IPacket;
 
 import java.nio.ByteBuffer;
@@ -24,26 +27,31 @@ public class Map implements IPacket{
 
     public static final int FINISH_SIZE = 50;
 
-    public static final int FINISH_X = 475;
-    public static final int FINISH_Y = 450;
-
-
     protected ArrayList<MySquare> squares;
-    protected Random myRand;
+    private Rectangle ending;
+    private Rectangle starting;
 
+    protected Random myRand;
+    private  int gameState = 0;
+
+    public  void  SetGamestate(int state)
+    {
+        this.gameState = state;
+    }
 
     //makes a random level with a random color out of the 6 prim/secondary colors
     public Map()
     {
         squares = new ArrayList(NUM_SQUARES );
         myRand = new Random();
+        ending = new Rectangle(MAP_WIDTH,MAP_HEIGHT/2.0f - 15f/2.0f,15,15);
+        starting = new Rectangle(0,0,200,MAP_HEIGHT);
     }
+
 
     public void GenerateMap()
     {
-
         randomize();
-
     }
 
 
@@ -59,17 +67,12 @@ public class Map implements IPacket{
             int rX = myRand.nextInt(MAP_WIDTH);
             int rY = myRand.nextInt(MAP_HEIGHT);
             int size = myRand.nextInt( MAX_SQUARE_SIZE - MIN_SQUARE_SIZE) + MAX_SQUARE_SIZE;
-            boolean darker = true;// myRand.nextBoolean();
-
-            //set random color newC for use
-            Color color = new Color();
 
             //create square and add to arraylist
             MySquare s = new MySquare(rX, rY, size, myRand.nextInt(360), myRand.nextInt(3)+1);
             squares.add(s);
         }
-        MySquare end = new MySquare(FINISH_X,FINISH_Y,FINISH_SIZE,0,4);
-        squares.add(end);
+
     }
 
     public  int GetSpeed(Vector2 point)
@@ -84,6 +87,55 @@ public class Map implements IPacket{
         return 0;
     }
 
+    public  void  UpdatePlayer(Player player,float delta)
+    {
+        if(gameState == GameState.WAITING)
+        {
+            if(player.Location.x > starting.x + starting.width)
+            {
+                player.Location.x = starting.x + starting.width;
+            }
+
+            if(player.Location.y > starting.y + starting.height)
+            {
+                player.Location.y = starting.y + starting.height;
+            }
+            if(player.Location.x < 0){
+                player.Location.x = 0;
+            }
+            if(player.Location.y < 0){
+                player.Location.y = 0;
+            }
+
+        }
+
+        float multiplier = 1;
+        int speed = this.GetSpeed(player.Location);
+        if(starting.contains(player.Location.x,player.Location.y))
+        {
+            multiplier = MySquare.FAST;
+        }
+        else {
+            switch (speed) {
+                case MySquare.SLOW:
+                    multiplier = .9f;
+                    break;
+                case MySquare.MEDIUM:
+                    multiplier = 1.2f;
+                    break;
+                case MySquare.FAST:
+                    multiplier = 3f;
+                    break;
+                case MySquare.SUPER_SLOW:
+                    multiplier = .3f;
+                    break;
+            }
+        }
+
+        Vector2 velocity = new Vector2(player.Velocity.x* delta *multiplier,player.Velocity.y* delta *multiplier);
+        player.Location = player.Location.add(velocity);
+    }
+
     public void draw(ShapeRenderer shape)
     {
         //System.out.println("SQUARES: " + squares.size());
@@ -92,6 +144,18 @@ public class Map implements IPacket{
         {
             squares.get(i).draw(shape);
         }
+
+        shape.begin(ShapeRenderer.ShapeType.Filled);
+        shape.identity();
+        shape.setColor(Color.GREEN);
+        shape.rect(ending.x, ending.y, ending.width / 2.0f, ending.height / 2.0f, ending.width, ending.height, 1, 1, 0.0f);
+        shape.end();
+
+        shape.begin(ShapeRenderer.ShapeType.Filled);
+        shape.identity();
+        shape.setColor(Color.WHITE);
+        shape.rect(starting.x,starting.y,starting.width,starting.height);
+        shape.end();
     }
 
     @Override
